998a1010b5865e4722577c15d66230947363325e
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2024-07-27 02:44:03 +0530

    adding two files to the project

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 
-var newName= "John";
-document.getElementById('demo').innerHTML = newName;
+var newName= "nice";
+var newName1 = "very nice";
+newName += newName1;
+var newName3 = newName[newName.length - 1];
+document.getElementById('demo').innerHTML = newName3;
