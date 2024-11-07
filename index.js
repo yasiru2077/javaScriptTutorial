23ba30ac951cfba552b3dd1df3ca371cf783f4f4
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2024-11-07 14:52:16 +0530

    refactoring files

--- index.js
+++ index.js
@@ -4,8 +4,40 @@ console.log("hey");
 let userAge;
 console.log(userAge);
 
-
 // null : value is assiged ("no value" or "empty value")
 let userEmail = null;
 console.log(userEmail);
+
+let isLooseEqual = 5 == "5";
+
+console.log(isLooseEqual);
+
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+const sayHi = function(name) {
+    return `Hi, ${name}!`;
+};
+
+let globalVar = "I'm global";
+
+function scopeExample() {
+    let localVar = "I'm local";
+    console.log(globalVar);  // Accessible
+    console.log(localVar);   // Accessible
+}
+
+function createCounter() {
+    let count = 0;
+    return function() {
+        return ++count;
+    };
+}
+
+const counter = createCounter();
+console.log(counter()); // 1
+console.log(counter()); // 2
+console.log(counter()); // 2
+
 
