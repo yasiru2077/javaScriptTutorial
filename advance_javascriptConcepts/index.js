d317b81cd459af03ca5864e06681f2cec65d7511
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2024-11-16 11:25:00 +0530

    refactoring files

--- advance_javascriptConcepts/index.js
+++ advance_javascriptConcepts/index.js
@@ -1,3 +1,9 @@
+//Advance javascript topics
+
+// closures and lexical scope
+
+//lexical scope
+
 function outer() {
   let outerVar = "I am outer";
   function inner() {
@@ -7,3 +13,48 @@ function outer() {
 }
 
 outer();
+
+// closure scope
+
+function outer() {
+  let count = 0;
+  return function inner() {
+    count++;
+    console.log(count);
+  };
+}
+const counter = outer();
+counter(); // 1
+counter(); // 2
+
+/* synchronous means code that executes line by line in 
+a sequential manner code that wait for an operation to complete*/
+
+// asynchronous allows operations to performed concurrently without waiting.
+// it does not block the execution flow and allows the program to continue.
+
+// asynchronous operations
+
+// timeouts
+
+console.log("Start");
+setTimeout(() => console.log("mid"), 3000);
+console.log("End");
+
+// callback
+
+function func1(callback) {
+  setTimeout(() => {
+    console.log("Task 1");
+    callback();
+  }, 3000);
+}
+
+function func2() {
+  console.log("Task 2");
+  console.log("Task 3");
+  console.log("Task 4");
+  console.log("Task 5");
+}
+
+func1(func2);
