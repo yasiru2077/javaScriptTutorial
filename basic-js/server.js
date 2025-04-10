89be4673b312d0d2911b61cdac0e8162347e95e3
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2025-04-10 13:21:05 +0530

    js basics

---
+++ basic-js/server.js
@@ -0,0 +1,18 @@
+const person = {
+  name: "John",
+  age: "30",
+  hobbies: ["reading", "gaming"],
+};
+
+function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+const message = greet(`Alice`);
+
+console.log(message);
+
+const add = (a, b) => a + b;
+
+add(2, 4)
+
