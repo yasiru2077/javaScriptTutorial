04d9597eafe28234f4533bc23502a06d78a182a2
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2025-04-10 14:42:52 +0530

    js basics

--- basic-js/server.js
+++ basic-js/server.js
@@ -14,5 +14,33 @@ console.log(message);
 
 const add = (a, b) => a + b;
 
-add(2, 4)
+add(2, 4);
+
+// loops
+const item = ["apple", "banana", "cherry"];
+
+const item1 = [
+  ["x", "y", "z"],
+  ["x1", "y1", "z1"],
+];
+
+for (let index = 0; index < item.length; index++) {
+  const element = item[index];
+  console.log(element);
+}
+
+item.forEach((item) => console.log(item));
+item1.map((i) => console.log("map",i));
+
+// Arrays & Objects
+
+const fruits = [`apple`, `banana`];
+
+fruits.push("cherry");
+
+console.log("second:", fruits[1]);
+
+const user = { name: "Tom", age: 22 };
+console.log(user.name);
+
 
