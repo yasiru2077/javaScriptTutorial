da27d900cf758b27fcd6b372675396e2ad9f6bff
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2025-05-08 17:20:49 +0530

    react hooks

--- react-hooks/src/App.jsx
+++ react-hooks/src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Usestate from "./pages/usestate";
+import Useeffect from "./pages/useeffect";
 
 function App() {
-
-
   return (
     <>
-      <div></div>
+      <Usestate />
+      <br />
+      <Useeffect />
     </>
   );
 }
--- react-hooks/src/pages/usestate.jsx
+++ react-hooks/src/pages/usestate.jsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { useState } from "react";
 
 function Usestate() {
-  return <div>Usestate</div>;
+  const [count, setCount] = useState(0);
+
+  const increaseCount = () => {
+    setCount((prev) => prev + 2);
+    setCount((prev) => prev + 2);
+  };
+
+  return (
+    <div>
+      <p>count:{count}</p>
+      <button onClick={increaseCount}>increase</button>
+    </div>
+  );
 }
 
 export default Usestate;
