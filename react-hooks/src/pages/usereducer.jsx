a519b18966ccf3f55e8c87431ac81ba8995ca1b3
yasiru2077 <96358931+yasiru2077@users.noreply.github.com>
2025-05-26 16:36:54 +0530

    adding redux tutorial

--- react-hooks/src/pages/usereducer.jsx
+++ react-hooks/src/pages/usereducer.jsx
@@ -1,7 +1,32 @@
-import React from "react";
+import React, { useReducer } from 'react'
 
-function Usereducer() {
-  return <div>Usereducer</div>;
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'increment':
+            return { count: state.count + 1 }
+        case 'decrement':
+            return { count: state.count - 1 }
+        case 'double':
+            return { count: state.count * 2 }
+        case 'reset':
+            return { count: 0 }
+        default:
+            return state;
+    }
+};
+
+function UseReducer() {
+    const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+    return (
+        <div>
+            <p>Count:{state.count}</p>
+            <button onClick={() => dispatch({ type: "increment" })}>+</button>
+            <button onClick={() => dispatch({ type: "decrement" })}>-</button>
+            <button onClick={() => dispatch({ type: "double" })}>*</button>
+            <button onClick={() => dispatch({ type: "reset" })}>reset</button>
+        </div>
+    )
 }
 
-export default Usereducer;
+export default UseReducer
